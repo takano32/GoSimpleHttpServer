940f96da7d22759a6547de2b2963a8982dadceb6
TAKANO Mitsuhiro <takano32@gmail.com>
2013-09-20 21:32:31 +0900

    add app.go.
    paste from http://golang.org/pkg/net/http/

---
+++ app.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"log"
+)
+
+// hello world, the web server
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "hello, world!\n")
+}
+
+func main() {
+	http.HandleFunc("/hello", HelloServer)
+	err := http.ListenAndServe(":12345", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+
