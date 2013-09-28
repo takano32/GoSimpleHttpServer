ee9b4af81adada7f2df0d8c398770881aad81d3c
TAKANO Mitsuhiro <takano32@gmail.com>
2013-09-28 14:55:15 +0900

    use http.FileServer.

--- SimpleHttpServer.go
+++ SimpleHttpServer.go
@@ -3,17 +3,14 @@ package main
 import (
 	"net/http"
 	"log"
-
-	"./handler"
 )
 
 
 func main() {
-	http.HandleFunc("/hello", handler.HelloServer)
+	http.Handle("/", http.FileServer(http.Dir(".")))
 	err := http.ListenAndServe(":12345", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
-
 
